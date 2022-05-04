0769afa606f0d5865624be4e50989f77e8245e4f
Toni Immonen <taimmone@outlook.com>
2022-05-04 07:57:35 +0300

    Make block move down one row per tick

--- src/Board.mjs
+++ src/Board.mjs
@@ -2,22 +2,28 @@ export class Board {
   width;
   height;
   falling;
+  ticks;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
     this.falling = null;
+    this.ticks = 0;
   }
 
   drop(block) {
     this.falling = block;
   }
 
+  tick() {
+    this.ticks++;
+  }
+
   toString() {
     let board = "";
     for (let row = 0; row < this.height; ++row) {
       for (let col = 0; col < this.width; ++col) {
-        if (this.falling && row === 0 && col === 1) {
+        if (this.falling && row === this.ticks && col === 1) {
           board = board + this.falling.color;
         } else {
           board = board + ".";
