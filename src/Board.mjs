966e2407e2e38ac428c250668963ea7647899d4b
Toni Immonen <taimmone@outlook.com>
2022-05-04 07:54:04 +0300

    Set falling block to start from the top middle

--- src/Board.mjs
+++ src/Board.mjs
@@ -1,17 +1,27 @@
 export class Board {
   width;
   height;
+  falling;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.falling = null;
+  }
+
+  drop(block) {
+    this.falling = block;
   }
 
   toString() {
     let board = "";
     for (let row = 0; row < this.height; ++row) {
       for (let col = 0; col < this.width; ++col) {
-        board = board + ".";
+        if (this.falling && row === 0 && col === 1) {
+          board = board + this.falling.color;
+        } else {
+          board = board + ".";
+        }
       }
       board = board + "\n";
     }
