809ce40a930d49a1c3daaaadfbc0e7d1617b49c8
Toni Immonen <taimmone@outlook.com>
2022-05-08 11:08:34 +0300

    Use RotatingShape to define T-shape for rotation

--- src/Tetromino.mjs
+++ src/Tetromino.mjs
@@ -1,3 +1,9 @@
+import { RotatingShape } from "./RotatingShape.mjs";
+
 export class Tetromino {
-  static T_SHAPE = ".T.\nTTT\n...\n";
+  static T_SHAPE = new RotatingShape(
+    `.T.
+     TTT
+     ...`
+  );
 }
