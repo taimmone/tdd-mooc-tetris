f124e71737dce3614f8fa22e68e03ce6e00e2209
Toni Immonen <taimmone@outlook.com>
2022-05-08 05:51:16 +0300

    Declare Board class fields as private

--- src/Board.mjs
+++ src/Board.mjs
@@ -1,22 +1,22 @@
 const EMPTY = ".";
 
 export class Board {
-  width;
-  height;
-  falling;
-  ticks;
-  board;
+  #width;
+  #height;
+  #falling;
+  #ticks;
+  #board;
 
   constructor(width, height) {
-    this.width = width;
-    this.height = height;
-    this.falling = null;
-    this.ticks = 0;
-    this.board = [...Array(height)].map(() => Array(width).fill(EMPTY));
+    this.#width = width;
+    this.#height = height;
+    this.#falling = null;
+    this.#ticks = 0;
+    this.#board = [...Array(height)].map(() => Array(width).fill(EMPTY));
   }
 
   hasFalling() {
-    return !!this.falling;
+    return !!this.#falling;
   }
 
   drop(block) {
@@ -27,12 +27,12 @@ export class Board {
   }
 
   setFalling(block) {
-    this.falling = block;
-    this.board[0][1] = block.color;
+    this.#falling = block;
+    this.#board[0][1] = block.color;
   }
 
   tick() {
-    this.ticks++;
+    this.#ticks++;
     if (this.hasFalling()) {
       this.moveFalling();
     }
@@ -40,33 +40,33 @@ export class Board {
 
   moveFalling() {
     let fallingPoint = null;
-    for (let row = 0; row < this.height; ++row) {
-      for (let col = 0; col < this.width; ++col) {
-        if (this.hasFalling() && this.board[row][col] == this.falling.color) {
+    for (let row = 0; row < this.#height; ++row) {
+      for (let col = 0; col < this.#width; ++col) {
+        if (this.hasFalling() && this.#board[row][col] == this.#falling.color) {
           fallingPoint = [...[row, col]];
         }
-        if (this.board[row][col] === this.falling.color) {
-          this.board[row][col] = EMPTY;
+        if (this.#board[row][col] === this.#falling.color) {
+          this.#board[row][col] = EMPTY;
         }
       }
     }
 
     if (fallingPoint) {
       const [row, col] = fallingPoint;
-      if (row + 1 >= this.height || this.board[row + 1][col] !== EMPTY) {
-        this.board[row][col] = this.falling.color;
-        this.falling = null;
+      if (row + 1 >= this.#height || this.#board[row + 1][col] !== EMPTY) {
+        this.#board[row][col] = this.#falling.color;
+        this.#falling = null;
         return;
       }
-      this.board[row + 1][col] = this.falling.color;
+      this.#board[row + 1][col] = this.#falling.color;
     }
   }
 
   toString() {
     let board = "";
-    for (let row = 0; row < this.height; ++row) {
-      for (let col = 0; col < this.width; ++col) {
-        board = board + this.board[row][col];
+    for (let row = 0; row < this.#height; ++row) {
+      for (let col = 0; col < this.#width; ++col) {
+        board = board + this.#board[row][col];
       }
       board = board + "\n";
     }
